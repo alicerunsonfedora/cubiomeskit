79f35562b011b0825537e4caa4c58724e25c8677
Marquis Kurt <software@marquiskurt.net>
2025-06-07 16:07:25 -0400

    🚧 Create MinecraftWorldRendererActor
    Will be used to isolate rendering tasks into its on group.
    
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>
    
    Co-authored-by: Grant Neufeld <activist@gmail.com>
    Co-authored-by: Joe Graf <joe.graf@gmail.com>
    Co-authored-by: Vlad Sima <vladgrigoresima10@gmail.com>
    Co-authored-by: Andy Riordan <andycr512@gmail.com>
    Co-authored-by: Femi Aliu <femialiu713@gmail.com>

---
+++ Sources/CubiomesKit/Rendering/MinecraftWorldRendererActor.swift
@@ -0,0 +1,11 @@
+//
+//  MinecraftWorldRendererActor.swift
+//  CubiomesKit
+//
+//  Created by Marquis Kurt on 07-06-2025.
+//
+
+/// A global actor used to handle world rendering tasks.
+@globalActor public actor MinecraftWorldRendererActor {
+    public static let shared = MinecraftWorldRendererActor()
+}
