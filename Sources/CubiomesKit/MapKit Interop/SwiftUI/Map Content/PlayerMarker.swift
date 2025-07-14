eede3e11b89af8f577b3f0fa65190f0bd679da28
Marquis Kurt <software@marquiskurt.net>
2025-07-14 01:29:20 -0400

    🚧 Add title and subtitle

--- Sources/CubiomesKit/MapKit Interop/SwiftUI/Map Content/PlayerMarker.swift
+++ Sources/CubiomesKit/MapKit Interop/SwiftUI/Map Content/PlayerMarker.swift
@@ -28,16 +28,32 @@ public class MinecraftMapPlayerMarkerAnnotation: NSObject, MKAnnotation {
     public private(set) var playerUUID: UUID
     public private(set) var coordinate: CLLocationCoordinate2D
 
+    /// The name of the marker.
+    public var title: String?
+
+    /// The subtitle of the marker, which displays the marker in Minecraft coordinates.
+    public private(set) var subtitle: String?
+
     public init(name: String, playerUUID: UUID, location: CGPoint) {
         self.name = name
         self.playerUUID = playerUUID
         self.coordinate = CLLocationCoordinate2D(projecting: location)
+
+        self.title = name
+        let xCoord = Int(location.x)
+        let zCoord = Int(location.y)
+        self.subtitle = "(\(xCoord), \(zCoord))"
     }
 
     public init(playerMarker: PlayerMarker) {
         self.name = playerMarker.name
         self.playerUUID = playerMarker.playerUUID
         self.coordinate = CLLocationCoordinate2D(projecting: playerMarker.location)
+
+        self.title = name
+        let xCoord = Int(playerMarker.location.x)
+        let zCoord = Int(playerMarker.location.y)
+        self.subtitle = "(\(xCoord), \(zCoord))"
     }
 
     public override func isEqual(_ object: Any?) -> Bool {
